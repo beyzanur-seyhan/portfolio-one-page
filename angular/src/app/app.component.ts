f155a88bed110025323e9f0bc125f1cbe257e13c
Beyzanur Seyhan <seyhanbeyzanur836@gmail.com>
2024-01-15 22:26:26 +0300

    add title and metatags

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import { Title, Meta } from '@angular/platform-browser';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -9,4 +9,16 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private titleService: Title, private metaService: Meta) {
+    this.titleService.setTitle('Beyzanur Seyhan | Frontend Developer');
+    this.metaService.addTags([
+      { name: 'keywords', content: 'frontend, software, developer' },
+      {
+        name: 'description',
+        content:
+          'Merhaba! Ben Beyzanur. Front-end geliştiricisiyim ve yaratıcı çözümler üretmek için tutku dolu biriyim. Portföyümde, bu alanda nasıl gelişiyor olduğumu keşfedin.',
+      },
+    ]);
+  }
+}
