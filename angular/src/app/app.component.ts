dda8fb7f4049dc76e5738db4a2bbf38e665c1402
Beyzanur Seyhan <seyhanbeyzanur836@gmail.com>
2024-01-15 23:23:43 +0300

    add navbar

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { Title, Meta } from '@angular/platform-browser';
+import { NavbarComponent } from './navbar/navbar.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
