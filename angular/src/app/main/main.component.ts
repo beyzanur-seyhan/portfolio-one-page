8edcec74a879f426be2e628518c1c8b277d0c8b9
Beyzanur Seyhan <seyhanbeyzanur836@gmail.com>
2024-01-16 20:42:49 +0300

    add child component and text split function

--- angular/src/app/main/main.component.ts
+++ angular/src/app/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ExperienceComponent } from './experience/experience.component';
@@ -8,10 +8,52 @@ import { ContactComponent } from './contact/contact.component';
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [HomeComponent, AboutComponent, ExperienceComponent, PortfolioComponent, ContactComponent],
+  imports: [
+    HomeComponent,
+    AboutComponent,
+    ExperienceComponent,
+    PortfolioComponent,
+    ContactComponent,
+  ],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.css'
+  styleUrl: './main.component.css',
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
+  initText() {
+    document.querySelectorAll('.txt-fx').forEach(function (element) {
+      let newstr = '';
+      let count = 0;
+      let delay = 100;
+      let stagger = 10;
+      let words = element.textContent?.split(/\s/);
+      let arrWords = new Array();
 
+      words?.forEach(function (value) {
+        newstr = '<span class="word">';
+
+        for (let i = 0, l = value.length; i < l; i++) {
+          newstr +=
+            "<span class='letter' style='transition-delay:" +
+            (delay + stagger * count) +
+            "ms;'>" +
+            value[i] +
+            '</span>';
+          count++;
+        }
+        newstr += '</span>';
+
+        arrWords.push(newstr);
+        count++;
+      });
+
+      element.innerHTML = arrWords.join(
+        "<span class='letter' style='transition-delay:" +
+          delay +
+          "ms;'>&nbsp;</span>"
+      );
+    });
+  }
+  ngOnInit(): void {
+    this.initText();
+  }
 }
