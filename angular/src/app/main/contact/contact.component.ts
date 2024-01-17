a3d4b9b2a0a8a330c6941952d16348fd00a448bd
Beyzanur Seyhan <seyhanbeyzanur836@gmail.com>
2024-01-17 18:20:21 +0300

    create contact component

--- angular/src/app/main/contact/contact.component.ts
+++ angular/src/app/main/contact/contact.component.ts
@@ -1,12 +1,38 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrl: './contact.component.css',
 })
 export class ContactComponent {
-
+  contactInformations: {
+    title: string;
+    link?: string;
+    description?: string;
+  }[] = [
+    {
+      title: 'Adres',
+      description: 'Nevşehir / Türkiye',
+    },
+    {
+      title: 'E-mail',
+      link: 'info@beyzanurseyhan.com',
+    },
+    {
+      title: 'Linkedin',
+      link: 'www.linkedin.com/in/beyzanurseyhan/',
+    },
+    {
+      title: 'Github',
+      link: 'github.com/beyzanur-seyhan',
+    },
+    {
+      title: 'Twitter',
+      link: 'twitter.com/minikfreelancer',
+    },
+  ];
 }
